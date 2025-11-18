45e67441e1e2b3db463a5765ba924368109cb28b
toulouse31-eng <toulouse31@gmail.com>
2025-11-18 14:42:17 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  clients.claim();
+});
+
+self.addEventListener("fetch", () => {
+  // laisse Chrome gérer le réseau
+});
